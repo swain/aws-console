033ffc4ad68ed2510f6e9f43dcec141bbac9c5aa
Swain Molster <swain.molster@gmail.com>
2025-08-02 08:32:55 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# aws-console
-Tiny utility for opening the aws-console via CLI
+A tiny utility for opening the aws-console via CLI.
+
+## Usage
+
+```bash
+npx @swain/aws-console {profile}
+# OR
+npm i @swain/aws-console
+npx aws-console {profile}
+```
+
+## Publishing
+
+This package is published manually. Run the following:
+
+```bash
+npm run build
+npm publish
+```
---
+++ index.ts
@@ -0,0 +1,46 @@
+import { encode } from "node:querystring";
+import { fromIni } from "@aws-sdk/credential-provider-ini";
+import open from "open";
+import { parseArgs } from "node:util";
+
+const main = async () => {
+  const { positionals: [profile] } = parseArgs({
+    strict: true,
+    allowPositionals: true,
+    args: process.argv.slice(2)
+  })
+
+  if (!profile) {
+    throw new Error('Missing profile argument. Usage: @swain/aws-console <profile>')
+  }
+
+  const identity = await fromIni({ profile })();
+
+  const response = await fetch(
+    `https://signin.aws.amazon.com/federation?${encode({
+      Action: "getSigninToken",
+      DurationSeconds: 43200,
+      Session: JSON.stringify({
+        sessionId: identity.accessKeyId,
+        sessionKey: identity.secretAccessKey,
+        sessionToken: identity.sessionToken,
+      }),
+    })}`,
+  ).then((res) => res.json());
+
+  const loginUrl = `https://signin.aws.amazon.com/federation?${encode(
+    {
+      Action: "login",
+      Destination: "https://us-east-1.console.aws.amazon.com/",
+      SigninToken: response.SigninToken,
+      Issuer: "https://example.com",
+    },
+  )}`;
+
+  await open(loginUrl, { wait: false });
+};
+
+main().catch((error) => {
+  console.error(error.message);
+  process.exitCode = 1;
+});
